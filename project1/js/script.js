7e7318b334a6b2bc771ab309e9904c046d3feaf4
Anton <savor.ua.gg@gmail.com>
2019-01-24 23:08:42 +0500

    point 009

--- project1/js/script.js
+++ project1/js/script.js
@@ -1,21 +1,60 @@
-let money, time;
-money = prompt("Ваш бюджет на месяц?");
-time = prompt("Введите дату в формате YYYY-MM-DD");
+let money = +prompt("Ваш бюджет на месяц?"),
+    time = prompt("Введите дату в формате YYYY-MM-DD");
 
 let appData = {
-    "money": money,
+    "budget": money,
     "timeData": time,
-    "expenses": {
-        ansver1: ansver2
-    },
-    optionalExpenses: null,
-    income: null,
-    savings: false
+    "expenses": {},
+    "optionalExpenses": {},
+    "income": [],
+    "savings": false,
+    "moneyPerDay": money / 30
 };
 
-var ansver1 = prompt("Введите обязательную статью расходов в этом месяце");
-var ansver2 = prompt("Во сколько обойдется?");
+for (let i = 0; i < 2; i++) {
+    let ans1 = prompt("Введите обязательную статью расходов в этом месяце");
+    let ans2 = prompt("Во сколько обойдется?");
+    if (ans1 == null || ans2 == null || ans1 == '' || ans2 == '' ||
+        ans1.length > 50 || ans2.length > 50) {
+        console.log("wrong data");
+        i--;
+    } else {
+        appData.expenses[ans1] = ans2;
+    }
+}
 
-alert("Бюджет на 1 день: " + parseInt(money) / 30);
+//Вариант цикла №2
+// let counter = 0;
+// while (counter < 2) {
+//     let ans1 = prompt("Введите обязательную статью расходов в этом месяце");
+//     let ans2 = prompt("Во сколько обойдется?");
+//     if (ans1 == null || ans2 == null || ans1 == '' || ans2 == '' ||
+//         ans1.length > 50 || ans2.length > 50) {
+//         console.log("wrong data");
+//         counter--;
+//     } else {
+//         appData.expenses[ans1] = ans2;
+//     }
+//     counter++;
+// }
+
+//Вариант цикла №3
+// let counter = 0;
+// do {
+//     let ans1 = prompt("Введите обязательную статью расходов в этом месяце");
+//     let ans2 = prompt("Во сколько обойдется?");
+//     if (ans1 == null || ans2 == null || ans1 == '' || ans2 == '' ||
+//         ans1.length > 50 || ans2.length > 50) {
+//         console.log("wrong data");        
+//     } else {
+//         appData.expenses[ans1] = ans2;
+//         counter++;
+//     }
+// }
+// while (counter < 2);
+
+
+
+alert("Бюджет на 1 день: " + appData.moneyPerDay);
 
 console.log("End of script");
