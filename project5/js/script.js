1e7281a7c8e0d4d269a4803d186efff74f73cfac
Anton2 <savor.ua.gg@gmail.com>
2019-02-14 20:54:19 +0500

    need fix timer

--- project5/js/script.js
+++ project5/js/script.js
@@ -1,38 +1,71 @@
-window.addEventListener("DOMContentLoaded", function() {
-    'use strict';
+window.addEventListener("DOMContentLoaded", function () {
+    "use strict";
 
     let header = document.querySelector(".info-header"),
         tabBtn = document.querySelectorAll(".info-header-tab"),
         tabContent = document.querySelectorAll(".info-tabcontent");
 
     function hideTabContent(beginFrom) {
-        for (let i = beginFrom; i < tabContent.length; i++){
-            if (!tabContent[i].classList.contains("hide")){
+        for (let i = beginFrom; i < tabContent.length; i++) {
+            if (!tabContent[i].classList.contains("hide")) {
                 tabContent[i].classList.add("hide");
             }
             tabContent[i].classList.remove("show");
         }
-    }   
+    }
 
-    function showTabContent(index){
+    function showTabContent(index) {
         tabContent[index].classList.remove("hide");
-        if (!tabContent[index].classList.contains("show")){
+        if (!tabContent[index].classList.contains("show")) {
             tabContent[index].classList.add("show");
         }
     }
 
     hideTabContent(1);
 
-    header.addEventListener("click", function(event) {
+    header.addEventListener("click", function (event) {
         let target = event.target;
-        if (target && target.classList.contains("info-header-tab")){
+        if (target && target.classList.contains("info-header-tab")) {
             hideTabContent(0);
-            for (let i = 0; i < tabBtn.length; i++){
-                if (tabBtn[i] == target){
+            for (let i = 0; i < tabBtn.length; i++) {
+                if (tabBtn[i] == target) {
                     showTabContent(i);
                     break;
                 }
             }
         }
     });
+
+    let deadLine = "2019-02-15";
+
+    function getTimeResidual(endtime){
+        let interval = Date.parse(endtime) - Date.parse(new Date());
+
+        return {
+            total : interval,
+            seconds : Math.floor((interval / 1000) % 60),
+            minutes : Math.floor((interval / 1000 / 60) % 60),
+            hours : Math.floor((interval / 1000 / 60 / 60) % 60) 
+        };
+    }
+
+    function setClock(timerId){
+        let timer = document.getElementById(timerId),
+            seconds = timer.querySelector(".seconds"),
+            minutes = timer.querySelector(".minutes"),
+            hours = timer.querySelector(".hours");
+        
+
+        function applyTime(time) {
+            hours.textContent = time.hours;
+            minutes.textContent = time.minutes;
+            seconds.textContent = time.seconds;
+            if (time.total <= 0) {
+                // clearInterval(intervalId);
+            }
+        }
+        
+        let intervalId = setInterval(applyTime(getTimeResidual(deadLine)), 1000);
+    }
+    setClock("timer");
 });
